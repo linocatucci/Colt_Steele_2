7495f3be913864996852fd51d42c03d29e91cefc
linocatucci <Lino@Fernie.local>
2017-03-24 20:03:35 +0100

    start met javascript, console.log, alert, prompt

---
+++ assignments/JS Beginnings/script.js
@@ -0,0 +1,16 @@
+console.log('dit is een test!');
+
+/*var firstName = prompt('What is your firstname?');
+var lastName = prompt('what is your lastname?');
+var age = prompt('what is your age?');
+
+console.log('hello ' + firstName + ' ' + lastName);
+console.log('Your age is: ' + age);
+
+alert('nice to meet you ' + firstName + ' ' + lastName);
+*/
+
+var age = prompt('What is your age?')
+
+var days = age * 365;
+alert(age + ' years is roughly ' + days + ' days');
