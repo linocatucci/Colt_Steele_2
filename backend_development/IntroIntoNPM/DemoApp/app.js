56f31259adca388d7472b9d3ff2afbb6a208153b
Lino Catucci <linocatucci@gmail.com>
2017-05-03 18:55:30 +0000

    2nd commit from cloud 9

---
+++ backend_development/IntroIntoNPM/DemoApp/app.js
@@ -0,0 +1,8 @@
+var catMe = require('cat-me');
+var knockJoke = require('knock-knock-jokes')
+
+console.log(catMe());
+
+// knock knock jokes
+console.log('Knock Knock jokes.')
+console.log(knockJoke());
