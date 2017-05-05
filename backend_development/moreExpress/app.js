e6dcd752892a8f8299200ecd999ac2042283630d
linocatucci <Lino@Fernie.local>
2017-05-05 16:12:32 +0200

    more Express templates

--- backend_development/moreExpress/app.js
+++ backend_development/moreExpress/app.js
@@ -1,19 +1,41 @@
 var express = require("express");
 var app = express();
-
+// tells express to use the public folder for css files
+app.use(express.static('public'));
+// don't have to use the .ejs extentsion
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
 
-    res.render('home.ejs');
+    res.render('home');
 });
 
 app.get('/fallinlovewith/:thing', function (req, res) {
     var thing = req.params.thing;
     // res.send('you fell in love with ' + thing);
-    res.render('love.ejs', {
+    res.render('love', {
         thingVar: thing
     })
 });
+
+app.get('/posts', function (req, res) {
+    var posts = [{
+            title: 'post 1',
+            author: 'Susy'
+        },
+        {
+            title: 'My adorable pet bunny',
+            author: 'Charlie'
+        },
+        {
+            title: 'Can you believe this pomsky?',
+            author: 'Colt'
+        }
+    ];
+    res.render('posts', {
+        posts: posts
+    });
+})
 
 // bij cloud 9 met je dit gebruiken, dit is geen hardcoded
 // app.listen(process.env.PORT, process.env.IP, function () {
