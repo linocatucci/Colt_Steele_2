a421d9a5683dbc5e87f001a387852d21a3c2a62d
linocatucci <Lino@Fernie.local>
2017-05-06 07:31:34 +0200

    /addfriend post request

--- backend_development/PostRequestDemo/app.js
+++ backend_development/PostRequestDemo/app.js
@@ -1,7 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs');
+
+var friends = ['Ton', 'Suzan', 'Justin', 'Julia', 'Erik'];
+
+app.get('/', function (req, res) {
+    console.log('somebody nav to root')
+    res.render('home');
+});
+
+app.get('/friends', function (req, res) {
+    res.render('friends', {
+        friends: friends
+    });
+})
+
+// posting data needs a post route
+app.post('/addfriend', function (req, res) {
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect('/friends');
+})
 
 app.listen('3000', function () {
     console.log('Post request Demo app started!')
