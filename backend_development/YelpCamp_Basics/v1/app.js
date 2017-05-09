d1cb68eb4eede9883dc97df6b6e0582ba28dc69d
linocatucci <Lino@Fernie.local>
2017-05-09 20:46:19 +0200

    small changes on layout and includes of bootstrap css en js

--- backend_development/YelpCamp_Basics/v1/app.js
+++ backend_development/YelpCamp_Basics/v1/app.js
@@ -7,34 +7,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// tell nodejs to use the public folder with js and css
 app.use(express.static('public'));
 
 var campgrounds = [{
-        name: 'Granite Hill',
-        image: 'https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg'
-    }, {
-        name: 'Devils Peak',
-        image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
-    }, {
-        name: 'Mosquito Creek',
-        image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'
-    }, {
-        name: 'Devils Peak',
-        image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
-    }, {
-        name: 'Devils Peak',
-        image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
-    },{
-        name: 'Mosquito Creek',
-        image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'
-    },{
-        name: 'Granite Hill',
-        image: 'https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg'
-    }, {
-        name: 'Devils Peak',
-        image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
-    }
-];
+    name: 'Granite Hill',
+    image: 'https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg'
+}, {
+    name: 'Devils Peak',
+    image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
+}, {
+    name: 'Mosquito Creek',
+    image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'
+}, {
+    name: 'Devils Peak',
+    image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
+}, {
+    name: 'Devils Peak',
+    image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
+}, {
+    name: 'Mosquito Creek',
+    image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'
+}, {
+    name: 'Granite Hill',
+    image: 'https://farm3.staticflickr.com/2923/13950231147_7032e443a0.jpg'
+}, {
+    name: 'Devils Peak',
+    image: 'https://farm1.staticflickr.com/661/32853307921_c804935e58.jpg'
+}];
 
 app.get('/', function (req, res) {
     console.log('This will be the landing page soon!');
@@ -70,12 +70,12 @@ app.get('/campgrounds/new', function (req, res) {
     res.render('new');
 })
 
-// bij cloud 9 met je dit gebruiken, dit is geen hardcoded
-app.listen(process.env.PORT, process.env.IP, function () {
-    console.log('Server has started for Express ESJ Assignment!')
-});
+// // bij cloud 9 met je dit gebruiken, dit is geen hardcoded
+// app.listen(process.env.PORT, process.env.IP, function () {
+//     console.log('Server has started for Express ESJ Assignment!')
+// });
 
 // lokaal gebruiken
-// app.listen('3000', function () {
-//     console.log('The YelpCamp Server has started!');
-// })
+app.listen('3000', function () {
+    console.log('The YelpCamp Server has started!');
+})
