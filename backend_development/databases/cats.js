0f6cb361333e6cb31d02ab2708907b157b01395d
linocatucci@gmail.com <linocatucci@gmail.com>
2017-05-24 12:00:19 +0000

    mongo, monogDB en mongoose with cats.js

--- backend_development/databases/cats.js
+++ backend_development/databases/cats.js
@@ -7,22 +7,46 @@ var catSchema = new mongoose.Schema({
     temperament: String
 });
 
-// adding a new cat to the db
+// adding a new cat to the cat db, which will be meervourd in cats
 var Cat = mongoose.model('Cat', catSchema);
 
-var george = new Cat({
-    name: 'Lotje',
-    age: 11,
-    temperament: 'Scared'
-});
+// var george = new Cat({
+//     name: 'Polly',
+//     age: 11,
+//     temperament: 'sweet'
+// });
 
-george.save(function (err, cat) {
-    if (err) {
-        console.log('SOMETHING WENT WRONG!!')
+// george.save(function (err, cat) {
+//     if (err) {
+//         console.log('SOMETHING WENT WRONG!!')
+//     } else {
+//         console.log('WE JUST SAVED A CAT TO THE DB:')
+//         console.log(cat);
+//     }
+// });
+
+
+// create and save cat in 1 method instead of first create and then save a cat
+Cat.create({
+    name:'Snuitje',
+    age: 6,
+    temperament:'nice'
+}, function(err, cat){
+    if(err){
+        console.log(err);
     } else {
-        console.log('WE JUST SAVED A CAT TO THE DB:')
         console.log(cat);
     }
-});
+})
 
 // retrieve all cats from the DB and console.log the cats
+Cat.find({}, function(err, cats){
+    
+    if(err){
+        console.log('Oh NO an error!');
+        console.log(err);
+    } else {
+        console.log('Retrieved cats');
+        console.log(cats);
+    }
+})
